2f7860432c3532d091933ad6e58fd320ccc99e82
xphreak5 <40873583+xphreak5@users.noreply.github.com>
2022-07-07 16:34:30 +0200

    identity func added

---
+++ functions/guides.js
@@ -0,0 +1,20 @@
+exports.handler = async (event, context) => {
+  const guides = [
+    { title: "Beat all Zelda Bosses Like a Boss", author: "Mario" },
+    { title: "Mario Kart Shortcuts You Never Knew Existed", author: "Luigi" },
+    { title: "Ultimate Street Fighter Guide", author: "chun-li" }
+  ]
+
+  if (context.clientContext.user) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(guides)
+    }
+  }
+
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ mssg: "You must be logged in to see this" })
+  }
+  
+}
--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,57 @@
+import { useContext, useEffect, useState } from 'react'
 import styles from '../styles/Guides.module.css'
+import AuthContext from "../stores/authContext"
+
 
 export default function Guides() {
+  const { user, authReady } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
+
+
+  useEffect(() => {
+    if (authReady) {
+      fetch("/.netlify/functions/guides", user && {
+        headers: {
+          Authorization: "Bearer " + user.token.access_token
+        }
+      })
+        .then(res => {
+          if (!res.ok) {
+            throw Error("You must be logged in to view this content")
+          }
+          return res.json()
+        })
+        .then(data => {
+          setGuides(data)
+          setError(null)
+        })
+        .catch(err => {
+          setError(err.message)
+          setGuides(null)
+        })
+      }
+  },[user, authReady])
+
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
+        </div>
+      )}
+
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>Written by { guide.author }</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quam ratione blanditiis eos suscipit quo sequi dicta laboriosam dolorum magnam ut expedita commodi placeat officiis voluptatem, quis velit excepturi eaque?</p>
+        </div>
+      ))}
+
     </div> 
   )
 }
